a7bcb9c5afc7367e020656638d5337d4bf641ae4
pratamawkelvin-png <pratamawkelvin@gmail.com>
2026-01-21 22:14:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,19 @@
 # ---------------------------------------
 
 
-
-# Cek syarat metode bisection
 if f(a) * f(b) > 0:
     print("Metode bisection tidak bisa digunakan!")
     print("f(a) dan f(b) harus berlainan tanda.")
+else:
+    iterasi = 0
+    c = a
+
+    print("\nIterasi |   a    |   b    |   c    |  f(c)")
+    print("--------------------------------------------")
+
+    while abs(f(c)) > tol:
+        c = (a + b) / 2
+        iterasi += 1
 
         print(f"{iterasi:7} | {a:.4f} | {b:.4f} | {c:.4f} | {f(c):.6f}")
 
